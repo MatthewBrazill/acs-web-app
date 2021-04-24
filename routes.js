9d949e63aba892b4a837dcb664a2365612f1df82
Matthew Brazill <matthew@brazill.de>
2021-04-24 21:01:48 +0100

    Updated to use objective paths.

--- routes.js
+++ routes.js
@@ -1,27 +1,28 @@
-"use strict"
+`use strict`
 
 //Imports
-const express = require("express");
+const express = require(`express`);
 const router = express.Router();
 
 //Router Setup
-router.get("/index", function (req, res) {
-    res.render("index", {
-        title: "Example Page",
-        img_link: "",
-        instance_ip: "",
-        instance_type: "",
-        public_ip: ""
+router.get(`/index`, function (req, res) {
+    res.render(`index`, {
+        title: `Example Page`,
+        img_link: `${__dirname}/resources/cat_pic.jpg`,
+        instance_ip: ``,
+        instance_type: ``,
+        public_ip: ``
     })
 });
 
-router.get("/monitoring", function (req, res) {
-    res.render("monitoring", {
-        title: "WebServer Monitoring",
-        img_link: "",
-        instance_ip: "",
-        instance_type: "",
-        public_ip: ""
+router.get(`/monitoring`, function (req, res) {
+    res.render(`monitoring`, {
+        title: `WebServer Monitoring`,
+        img_link: ``,
+        instance_ip: ``,
+        instance_type: ``,
+        access_log: ``,
+        public_ip: ``
     })
 });
 
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-"use strict"
+`use strict`
 
 //Imports
-const express = require("express");
-const hbs = require("express-handlebars");
-const routes = require("./routes.js");
+const express = require(`express`);
+const hbs = require(`express-handlebars`);
+const routes = require(`${__dirname}/routes.js`);
 
 const app = express();
 
 //Set up the express framework
-app.engine(".hbs", hbs({
-    extname: ".hbs"
+app.engine(`.hbs`, hbs({
+    extname: `.hbs`
 }));
 app.use(express.urlencoded({extended: false}));
-app.set("view engine", ".hbs");
-app.use("/", routes);
+app.set(`view engine`, `.hbs`);
+app.use(`/`, routes);
 app.listen(80);
