c16d0ffa0355de981b24f4b001516208f3ce007a
Matthew Brazill <matthew@brazill.de>
2021-04-24 22:06:42 +0100

    Changed views directory path

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const app = express();
 
 //Set up the express framework
 app.engine(`.hbs`, hbs({
+
     extname: `.hbs`
 }));
 app.use(express.urlencoded({extended: false}));
+app.set(`views`, `${__dirname}/views`)
 app.set(`view engine`, `.hbs`);
 app.use(`/`, routes);
 app.listen(80);
